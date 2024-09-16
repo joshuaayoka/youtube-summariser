992dfe6d7c130e680044229b8543e5900b9ba8a9
joshuaayoka <joshuaayoka1@gmail.com>
2024-09-16 19:37:01 +0100

    Revert "summary creation for documents implemented"
    This reverts commit 767e2f8b7f5a9a80eb2031b9d5418e2a6c300c02.

--- server/hello.js
+++ server/hello.js
@@ -1,7 +1,5 @@
-const fs = require("fs")
+const ytt = require('youtube-transcript')
+const transcript = ytt.YoutubeTranscript.fetchTranscript("https://www.youtube.com/watch?v=QaNcP-WARt8").then(console.log)
 
-const extractTextFromTxt = (filePath) => {
-    return fs.readFileSync(filePath, 'utf8');
-};
-
-console.log(extractTextFromTxt('qlearning.txt'))
+// const ytt = require('./yt-transcript')
+// const transcript = ytt.YoutubeTranscript.fetchTranscript("https://www.youtube.com/watch?v=QaNcP-WARt8").then(console.log)

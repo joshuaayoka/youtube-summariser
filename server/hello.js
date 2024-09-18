0380539a994c137d30f194716614001c3144d968
joshuaayoka <joshuaayoka1@gmail.com>
2024-09-18 22:50:14 +0100

    remove more files

--- server/hello.js
+++
@@ -1,5 +0,0 @@
-const ytt = require('youtube-transcript')
-const transcript = ytt.YoutubeTranscript.fetchTranscript("https://www.youtube.com/watch?v=QaNcP-WARt8").then(console.log)
-
-// const ytt = require('./yt-transcript')
-// const transcript = ytt.YoutubeTranscript.fetchTranscript("https://www.youtube.com/watch?v=QaNcP-WARt8").then(console.log)

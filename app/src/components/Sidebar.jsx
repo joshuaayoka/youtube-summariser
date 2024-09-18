447d65fbbe264c7af0fc3c781177adc9e5528417
joshuaayoka <joshuaayoka1@gmail.com>
2024-09-18 22:20:59 +0100

    remove redundant files and reformat

--- app/src/components/Sidebar.jsx
+++ app/src/components/Sidebar.jsx
@@ -1,45 +1,47 @@
-import Button from "./Button"
-
 export default function Sidebar(props) {
-  
-    // Check if props.summaries is not empty
-    const hasSummaries = props.summaries && props.summaries.length > 0
-  
-    // If not empty, create buttons
-    const buttons = hasSummaries
-        ? props.summaries.map((summary) => {
-            const id = summary.id
-            return (
-                <div 
-                    key={`btn${id}`}
-                    onClick={() => props.setCurrentSummaryId(id)}
-                    id="select-summary"
-                    >
-                        <strong>{summary.title}</strong>
-                        <br></br>
-                        <br></br>
-                        <span id="datetime"><strong>Date:</strong> {summary.date}</span>
-                        <br></br>
-                        <span id="datetime"><strong>Time:</strong> {summary.time}</span>
+  // Check if props.summaries is not empty
+  const hasSummaries = props.summaries && props.summaries.length > 0;
 
-                        <Button 
-                            className="delete-summary"
-                            handleClick={(event) => props.deleteSummary(event, id)}
-                            text="Del"
-                            type="delete"
-                        />
-                </div>
-            )
-            })
-        : null
-  
-    return (
-        <aside
-            ref={props.sidebarRef}
-            className={`sidebar ${props.sidebarToggled ? "visible" : ""}`}
-        >
-            {buttons}
-        </aside>
-    );
-  }
-  
+  // If not empty, create buttons
+  const buttons = hasSummaries
+    ? props.summaries.map((summary) => {
+        const id = summary.id;
+        return (
+          <div
+            key={`btn${id}`}
+            onClick={() => props.setCurrentSummaryId(id)}
+            id="select-summary"
+          >
+            <strong>{summary.title}</strong>
+            <br />
+            <br />
+            <span id="datetime">
+              <strong>Date:</strong> {summary.date}
+            </span>
+            <br />
+            <span id="datetime">
+              <strong>Time:</strong> {summary.time}
+            </span>
+
+            <button
+              id="delete-button"
+              className="delete-summary"
+              onClick={(event) => props.deleteSummary(event, id)}
+              type="button" // Use type="button" to prevent accidental form submissions
+            >
+              Del
+            </button>
+          </div>
+        );
+      })
+    : null;
+
+  return (
+    <aside
+      ref={props.sidebarRef}
+      className={`sidebar ${props.sidebarToggled ? "visible" : ""}`}
+    >
+      {buttons}
+    </aside>
+  );
+}
--- app/src/components/Summary.jsx
+++ app/src/components/Summary.jsx
@@ -1,77 +1,74 @@
-import { useState, useId } from "react"
-import Button from "./Button"
-
+import { useState, useId } from "react";
 
 export default function Summary(props) {
-    const id = useId()
-    const videoId = props.summary.videoId
+  const id = useId();
+  const videoId = props.summary.videoId;
 
-    console.log(props.summary)
-    
-    const summaryElements = props.summary.text.map(text => {
-        return (
-            <div key={id + text.subtitle.charAt(0)}>
-                <h3>{text.subtitle}</h3>
-                <p>{text.content}</p>
-            </div>
-        )
-    })
+  console.log(props.summary);
 
-    function summaryToText() {
-        const summaryText = props.summary.text.reduce((text, item) => {
-            // Add subtitle and content to the text block
-            text += `${item.subtitle}\n${item.content}\n\n`;
-            return text;
-        }, '')        
-        return summaryText
-    }
-
+  const summaryElements = props.summary.text.map((text) => {
     return (
-        <div>
-            {props.summary.type === "youtube" &&
-            <div className="video-container">
-                {/* Video details on the left (or top on smaller screens) */}
-                <div className="video-details">
-                    <p className="video-title">Video: {props.summary.title}</p>
-                    <p>Creator: {props.summary.creator}</p>
-                </div>
-                
-                {/* Video iframe on the right (or below on smaller screens) */}
-                <div className="video-embed">
-                    <iframe
-                        src={`https://www.youtube.com/embed/${videoId}`}
-                        title={props.summary.title}
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                    ></iframe>
-                </div>
-            </div>
-            }
-            {props.summary.type === "document" &&
-            <div>
-                <div className="video-details">
-                    <p className="video-title">Title: {props.summary.title}</p>
-                </div>
-            </div>
-            }
-            <div id="info" className="summary-details">
-                <div>
-                    <h2>Summary</h2>
-                    <div>
-                        {summaryElements}
-                    </div>
-                </div>
-                <div className="download-container">         
-                    <Button
-                        className="download-summary"
-                        text="Download Summary"
-                        handleClick={() => props.handleDownload(
-                            summaryToText(),
-                            props.summary.title
-                        )}
-                    />
-                </div>
-            </div>
+      <div key={id + text.subtitle.charAt(0)}>
+        <h3>{text.subtitle}</h3>
+        <p>{text.content}</p>
+      </div>
+    );
+  });
+
+  function summaryToText() {
+    const summaryText = props.summary.text.reduce((text, item) => {
+      // Add subtitle and content to the text block
+      text += `${item.subtitle}\n${item.content}\n\n`;
+      return text;
+    }, "");
+    return summaryText;
+  }
+
+  return (
+    <div>
+      {props.summary.type === "youtube" && (
+        <div className="video-container">
+          {/* Video details on the left (or top on smaller screens) */}
+          <div className="video-details">
+            <p className="video-title">Video: {props.summary.title}</p>
+            <p>Creator: {props.summary.creator}</p>
+          </div>
+
+          {/* Video iframe on the right (or below on smaller screens) */}
+          <div className="video-embed">
+            <iframe
+              src={`https://www.youtube.com/embed/${videoId}`}
+              title={props.summary.title}
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>
         </div>
-    )
+      )}
+      {props.summary.type === "document" && (
+        <div>
+          <div className="video-details">
+            <p className="video-title">Title: {props.summary.title}</p>
+          </div>
+        </div>
+      )}
+      <div id="info" className="summary-details">
+        <div>
+          <h2>Summary</h2>
+          <div>{summaryElements}</div>
+        </div>
+        <div className="download-container">
+          <button
+            id="button"
+            className="download-summary"
+            onClick={() =>
+              props.handleDownload(summaryToText(), props.summary.title)
+            }
+          >
+            Download Summary
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
